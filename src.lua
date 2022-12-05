160165d663ecd9bc5b1d7ce353e1a970ea1782eb
Player788 <110638503+Player788@users.noreply.github.com>
2022-12-06 00:36:09 +0500

    Update src.lua

--- src.lua
+++ src.lua
@@ -1,4 +1,4 @@
-_G.Version = "1I"
+_G.Version = "1J"
 local Library = {
 	Flags = {},
 	Logs = {},
@@ -102,15 +102,13 @@ end
 function Library:Save()
 	local Data = {}
 	for i,v in pairs(Library.Flags) do
-		if v.Type then
-			if v.Type == "Colorpicker" then
-				Data[i] = {R = v.Value.R * 255, G = v.Value.G * 255, B = v.Value.B * 255}
-			end
+		if v.Type == "Colorpicker" then
+			Data[i] = {R = v.Value.R * 255, G = v.Value.G * 255, B = v.Value.B * 255}
 		else
-			Data[i] = v.Value
+			Data[i] = v.Value or v.EnumItem or v.Default
 		end
+		writefile(Library.Config.Saves.Folder .. "/" .. tostring(game.GameId) .. ".txt", tostring(HttpService:JSONEncode(Data)))
 	end
-	writefile(Library.Config.Saves.Folder .. "/" .. tostring(game.GameId) .. ".txt", tostring(HttpService:JSONEncode(Data)))
 end
 function Library:Load()
 	if isfile(Library.Config.Saves.Folder .. "/" .. game.GameId .. ".txt") then
